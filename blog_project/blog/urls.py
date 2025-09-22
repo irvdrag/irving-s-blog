f09585308a735f3b2c3d68ddf26809fce93b1352
MBB PRESEA AM <mWX1339082@china.huawei.com>
2025-09-21 23:14:44 -0600

    pendiente agregar login ya se desloguea y no deja editar

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.lista_posts, name='lista_posts'),
     path('post/<int:post_id>/', views.detalle_post, name='detalle_post'),
     path('crear/', views.crear_post, name='crear_post'), 
     path('post/<int:post_id>/editar/', views.editar_post, name='editar_post'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Inf about login and log out 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'lista_posts'
+LOGOUT_REDIRECT_URL = 'lista_posts'
